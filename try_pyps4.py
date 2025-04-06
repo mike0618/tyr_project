f9dde1aad591d82a0bef611910f0d12f235fe4af
Mike Zubko <mishau7@gmail.com>
2025-04-06 02:16:00 -0600

    add comments to pyps4

--- try_pyps4.py
+++ try_pyps4.py
@@ -17,26 +17,46 @@ class MyController(Controller):
         print("Goodbye world")
 
     def on_R3_down(self, value):
-        value /= 128  # change the range to 0-255
-        value += 51
-        value /= 1.2  # change the range to 51-255
-        value = int(value)
+        # Max value of remote range is 32767
+        # This calculation is needed because numbers from the range of 0 - 255 can be used when determining wheel speed
+        value /= 128  # Brings number down to the 255 range
+        value += 51  # Adds 51 to change the beginning of range
+        value /= 1.2  # Brings number back down to 255 range
+        value = int(
+            value
+        )  # Gets rid of decimal points because the bus only takes whole numbers
+        # Gives the controller a dead zone which helps the wheel stop
         if value < 50:
             value = 0
-        bus.write_i2c_block_data(motor2040_addr, 0x01, [value])
-        # Register 0x01 for backward
+        # Makes the wheel turn the speed of the value given by controller
+        bus.write_i2c_block_data(motor2040_addr, 0x01, [value])  # Register 0x01
+        # Retuns value for trouble shooting
         print(value)
 
     def on_R3_up(self, value):
+        # Max value of remote is -32767
+        # We do this to make the number positive for the wheel speed.
         value *= -1
-        value /= 128
-        value += 51
-        value /= 1.2
-        value = int(value)
+        # Max value of remote range is 32767
+        # This calculation is needed because numbers from the range of 0 - 255 can be used when determining wheel speed
+        value /= 128  # Brings number down to the 255 range
+        value += 51  # Adds 51 to bring to to 306
+        value /= 1.2  # Brings number back down to 255 range
+        value = int(
+            value
+        )  # Gets rid of decimal points because the bus only takes whole numbers
+        # Gives the controller a dead zone for the wheel to stop
         if value < 50:
             value = 0
+        # Makes the wheel turn the speed of the value given by controller
         bus.write_i2c_block_data(motor2040_addr, 0x00, [value])  # Register 0x00
+        # Retuns value for trouble shooting
         print(value)
+
+    # Stops the controller when the thumb stick is at rest
+    def on_R3_y_at_rest(self):
+        bus.write_i2c_block_data(motor2040_addr, 0x00, [0])  # Register 0x00
+        print("Stop")
 
     def on_L3_up(self, value):
         print(value)
